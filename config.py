4e9589ad9448d0650d4ea7ac476670f98df132fb
siowyl-wm22-lang <siowyl-wm22@student.tarc.edu.my>
2026-08-20 14:03:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c1maswoaqowi.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "siowyinglong-employee"
+customregion = "us-east-1"
+
